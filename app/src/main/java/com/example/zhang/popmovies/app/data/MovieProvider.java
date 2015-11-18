ad5b819375bbad8fda78e366a3110393e2aec4a2
zhang <xyzhang0817@gmail.com>
2015-11-17 21:53:37 -0500

    Setting UriMathcer.

--- app/src/main/java/com/example/zhang/popmovies/app/data/MovieProvider.java
+++ app/src/main/java/com/example/zhang/popmovies/app/data/MovieProvider.java
@@ -2,6 +2,7 @@ package com.example.zhang.popmovies.app.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
@@ -9,7 +10,21 @@ import android.support.annotation.Nullable;
 /**
  * Created by zhang on 16/11/15.
  */
-public class MovieProvider extends ContentProvider{
+public class MovieProvider extends ContentProvider {
+
+    private MovieDbHelper mOpenHelper;
+
+    static final int MOVIE = 100;
+    static final int MOVIE_WITH_MOVIE_ID = 200;
+
+    static UriMatcher buildUriMatcher() {
+        final UriMatcher mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+        mUriMatcher.addURI(authority, MovieContract.PATH_MOVIE, MOVIE);
+        mUriMatcher.addURI(authority, MovieContract.PATH_MOVIE + "#", MOVIE_WITH_MOVIE_ID);
+        return mUriMatcher; 
+    }
+
     @Override
     public boolean onCreate() {
         return false;
