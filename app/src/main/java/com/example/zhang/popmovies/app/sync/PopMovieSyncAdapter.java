e20242680c0b41bda31524fc0272e27985bd8099
zhang <xyzhang0817@gmail.com>
2016-02-05 18:05:54 -0500

    Finishing notification.

--- app/src/main/java/com/example/zhang/popmovies/app/sync/PopMovieSyncAdapter.java
+++ app/src/main/java/com/example/zhang/popmovies/app/sync/PopMovieSyncAdapter.java
@@ -2,19 +2,27 @@ package com.example.zhang.popmovies.app.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.SyncRequest;
 import android.content.SyncResult;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
+import com.example.zhang.popmovies.app.MainActivity;
 import com.example.zhang.popmovies.app.R;
 import com.example.zhang.popmovies.app.Utility;
 import com.example.zhang.popmovies.app.data.MovieContract;
@@ -43,6 +51,19 @@ public class PopMovieSyncAdapter extends AbstractThreadedSyncAdapter {
     public static final int SYNC_INTERVAL = 60 * 60 * 24;
     public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
 
+    // Interval to make the notification if there is no updates
+    public static final int SEVEN_DAYS_IN_MILLIS = 7 * 1000 * 60 *60 *24;
+    public static final int MOVIE_NOTIFICATION_ID = 3005;
+
+
+    // Information used in notification
+    private static final String[] NOTIFY_PREVIEW_PROJECTION = {
+            MovieContract.MovieEntry._ID,
+            MovieContract.MovieEntry.COLUMN_ORIGINAL_TITLE
+    };
+
+    private static final int COL_MOVIE_ID = 0;
+    private static final int COL_MOVIE_ORIGINAL_TITLE = 1;
 
     public PopMovieSyncAdapter(Context context, Boolean autoInitialize) {
         super(context, autoInitialize);
@@ -283,6 +304,8 @@ public class PopMovieSyncAdapter extends AbstractThreadedSyncAdapter {
 
             cursor.close();
 
+            notifyWeather();
+
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
             e.printStackTrace();
@@ -380,4 +403,80 @@ public class PopMovieSyncAdapter extends AbstractThreadedSyncAdapter {
     public static void initializeSyncAdapter (Context context) {
         getSyncAccount(context);
     }
+
+    public void notifyWeather() {
+        Context context = getContext();
+        // checking the last update and notify if it's the first of the day
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String lastNotificationKey = context.getString(R.string.pref_last_notification);
+        long lastSync = preferences.getLong(lastNotificationKey, 0);
+
+        if (System.currentTimeMillis() - lastSync >= SEVEN_DAYS_IN_MILLIS) {
+            // last sync is more than 7 days
+            String sortMethod = Utility.getPreferredSortMethod(context);
+            String sPreviewSelection = null;
+            String sortOrder = null;
+
+            if (sortMethod.equals(context.getString(R.string.sort_entryValue_popularity))) {
+                sPreviewSelection = MovieContract.MovieEntry.COLUMN_IS_POPULARITY + "=?";
+                sortOrder = MovieContract.MovieEntry.COLUMN_POPULARITY + " DESC";
+            } else if (sortMethod.equals(context.getString(R.string.sort_entryValue_highestRate))) {
+                sPreviewSelection = MovieContract.MovieEntry.COLUMN_IS_HIGHEST_RATE + "=?";
+                sortOrder = MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE + " DESC";
+            } else if (sortMethod.equals(context.getString(R.string.sort_entryValue_favourite))) {
+                sPreviewSelection = MovieContract.MovieEntry.COLUMN_IS_FAVOURITE + "=?";
+            }
+
+            Cursor cursor = context.getContentResolver().query(
+                    MovieContract.MovieEntry.CONTENT_URI,
+                    NOTIFY_PREVIEW_PROJECTION,
+                    sPreviewSelection,
+                    new String[]{"1"},
+                    sortOrder
+            );
+
+            if (cursor.moveToFirst()) {
+                String originalTitle = cursor.getString(COL_MOVIE_ORIGINAL_TITLE);
+                String contentText = String.format(
+                        context.getString(R.string.format_notification),
+                        originalTitle
+                );
+
+                // NotificationCompatBuilder is a very convenient way to build backward-compatible
+                // notifications.  Just throw in some data.
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(getContext())
+                                .setContentTitle(originalTitle);
+
+                // Make something interesting happen when the user clicks on the notification.
+                // In this case, opening the app is sufficient.
+                Intent resultIntent = new Intent(context, MainActivity.class);
+
+                // The stack builder object will contain an artificial back stack for the
+                // started Activity.
+                // This ensures that navigating backward from the Activity leads out of
+                // your application to the Home screen.
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(
+                                0,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                mBuilder.setContentIntent(resultPendingIntent);
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                // MOVIE_NOTIFICATION_ID allows you to update the notification later on.
+                mNotificationManager.notify(MOVIE_NOTIFICATION_ID, mBuilder.build());
+                
+                //refreshing last sync
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putLong(lastNotificationKey, System.currentTimeMillis());
+                editor.commit();
+            }
+
+        }
+    }
+
 }
